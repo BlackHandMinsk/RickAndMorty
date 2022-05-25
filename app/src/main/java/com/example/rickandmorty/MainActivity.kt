60ea4f5f7805f50452307bfe08bb8b17b7e81863
BlackHandMinsk <3058079@gmail.com>
2022-05-25 21:24:21 +0300

    feat:create main activity

---
+++ app/src/main/java/com/example/rickandmorty/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.rickandmorty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import com.example.rickandmorty.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.NavHostFragment)
+        return navController.navigateUp()
+    }
+}
